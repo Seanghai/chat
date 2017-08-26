0f3eedbc009af015e8b305131d7a6d901580ccc8
Seanghai <cheuseanghai23@gmail.com>
2017-08-26 23:25:43 +0700

    user profile

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 // Connect to Mongoose
-mongoose.connect('mongodb://localhost:27017/appchat', { useMongoClient: true });
+// mongoose.connect('mongodb://localhost:27017/appchat', { useMongoClient: true });
 
 // connect to mongodb on mlap.com
-// mongoose.connect('mongodb://@ds127963.mlab.com:27963/appchat', {user: 'seanghai', pass: 'Seanghai@123'});
+mongoose.connect('mongodb://@ds127963.mlab.com:27963/appchat', {user: 'seanghai', pass: 'Seanghai@123'});
 const db = mongoose.connection;
