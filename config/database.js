c41b91cbff1f04802646f99881712c08b210ed36
Seanghai <seanghai2306@gmail.com>
2017-08-28 10:58:51 +0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -3,5 +3,5 @@ const mongoose = require('mongoose');
 // mongoose.connect('mongodb://localhost:27017/appchat', { useMongoClient: true });
 
 // connect to mongodb on mlap.com
-mongoose.connect('mongodb://@ds127963.mlab.com:27963/appchat', {user: 'seanghai', pass: 'Seanghai@123'});
+mongoose.connect('mongodb://@ds127963.mlab.com:27963/appchat', {useMongoClient: false, user: 'seanghai', pass: 'Seanghai@123'});
 const db = mongoose.connection;
