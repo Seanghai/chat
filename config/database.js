8b12d20d915b15a44a63cce316393d50c7abc204
Seanghai <cheuseanghai23@gmail.com>
2017-08-28 11:50:28 +0700

    edit uri mongoose

--- config/database.js
+++ config/database.js
@@ -3,5 +3,6 @@ const mongoose = require('mongoose');
 // mongoose.connect('mongodb://localhost:27017/appchat', { useMongoClient: true });
 
 // connect to mongodb on mlap.com
-mongoose.connect('mongodb://@ds127963.mlab.com:27963/appchat', {useMongoClient: false, user: 'seanghai', pass: 'Seanghai@123'});
+// mongoose.connect('mongodb://@ds127963.mlab.com:27963/appchat', {user: 'seanghai', pass: 'Seanghai@123'});
+mongoose.connect('mongodb://sh:123@ds127963.mlab.com:27963/appchat', { useMongoClient: true });
 const db = mongoose.connection;
